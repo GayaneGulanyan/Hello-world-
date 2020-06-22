5084dc3468e931833d5049eb31be49fbc1b2d5f4
GayaneGulanyan <66916093+GayaneGulanyan@users.noreply.github.com>
2020-06-22 16:42:31 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Yooohooooo!");// write your code here
+        System.out.println("Hi, this is my first Java Lesson!");
+    }
+}
